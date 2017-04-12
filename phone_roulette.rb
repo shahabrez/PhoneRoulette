405905da184c8a408a4f6c49c4cadf019397236f
shahabrez <shahab.rezaie@gmail.com>
2017-04-12 14:38:35 +0200

    Add files via upload

--- phone_roulette.rb
+++ phone_roulette.rb
@@ -4,4 +4,11 @@ class PhoneRoulette < Sinatra::Base
   get "/" do
     "Hello World 2"
   end
+## phone_roulette.rb ##
+get_or_post '/roulette.xml' do
+  if flip_even_odd
+    erb :agent_call, :content_type => :xml
+  else
+    erb :customer_call, :content_type => :xml
+  end
 end
